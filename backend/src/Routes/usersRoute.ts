9229d2230c858150013f29bc77d5415f2320fd4e
FBetulDemir <fatmabetuld@gmail.com>
2025-09-25 11:19:03 +0200

    Added ScanCommand for retrieving dynamoDB data to see if setup working

--- backend/src/Routes/usersRoute.ts
+++ backend/src/Routes/usersRoute.ts
@@ -1,13 +1,26 @@
-import express, { Request, Response } from "express";
-import { DynamoDBClient, PutItemCommand, GetItemCommand } from "@aws-sdk/client-dynamodb";
-
+import express, { type Request, type Response } from "express";
+import {
+  DynamoDBClient,
+  PutItemCommand,
+  GetItemCommand,
+  ScanCommand,
+} from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const router = express.Router();
 const client = new DynamoDBClient({ region: "eu-north-1" });
+const ddb = DynamoDBDocumentClient.from(client);
+const table = process.env.TABLE_NAME!;
 
-
-router.post("/", async (req: Request, res: Response) => {
-
+router.get("/", async (req: Request, res: Response) => {
+  try {
+    const result = await ddb.send(new ScanCommand({ TableName: table }));
+    res.json(result.Items);
+  } catch (error) {
+    res.status(500).json({ error: "Could not retrieve users" });
+  }
 });
 
 export default router;

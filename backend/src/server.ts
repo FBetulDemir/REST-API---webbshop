d29a72b37c869871c43c8e85b7f8e468d5bfb85b
paria <pariyataba@gmail.com>
2025-09-25 10:45:30 +0200

    set up product route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express"
 const app=express()
 import dotenv from "dotenv"
+import productRouter from "./Routes/productsRoute.js"
 dotenv.config()
-
 const port=process.env.PORT
+
+app.use("/products",productRouter)
+
 
 app.listen(port,()=>{
 	console.log(`server run on port ${port}`)

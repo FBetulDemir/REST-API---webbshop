97ffa7942730d1c41bab36a51ae57c280c52045d
paria <pariyataba@gmail.com>
2025-09-29 10:18:30 +0200

    add post endpoint for products

--- backend/src/Routes/productsRoute.ts
+++ backend/src/Routes/productsRoute.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
+import express from "express"
 import type { Request, Response } from "express";
 import { ddbDocClient } from "../data/dynamoDb.js";
-import { ScanCommand } from "@aws-sdk/lib-dynamodb"; // note: lib-dynamodb
+import { PutCommand, ScanCommand } from "@aws-sdk/lib-dynamodb"; 
 import type { Product } from "../data/types/produstsType.js";
-import { productsArraySchema } from "../data/validators/productValidate.js";
+import { createProductSchema, productsArraySchema } from "../data/validators/productValidate.js";
 
 const router = Router();
 const myTable = "webshop";
+router.use(express.json())
 
 router.get("/", async (req: Request, res: Response) => {
 	const params = {
@@ -28,5 +30,32 @@ router.get("/", async (req: Request, res: Response) => {
 		res.status(500).json({ message: "Something went wrong" });
 	}
 });
+router.post("/",async (req: Request, res: Response)=>{
+	try{
+		const validatedData = createProductSchema.parse(req.body);
+		const {id}=req.body
+		if (!id || typeof id !== "number") {
+			return res.status(400).json({ message: "Product id (number) is required" });
+		}
+
+		const newProduct={
+			pk:`PRODUCT#${id}`,
+			sk:"#METADATA",
+			...validatedData
+		}
+
+		await ddbDocClient.send(
+			new PutCommand({ TableName: myTable, Item: newProduct })
+		);
+		
+		res.status(201).json(newProduct);
+		
+	}catch(error){
+		console.log(error)
+		res.status(500).json({ message: "Something went wrong" });
+	}
+	
+	
+})
 
 export default router;
--- backend/src/data/validators/productValidate.ts
+++ backend/src/data/validators/productValidate.ts
@@ -3,9 +3,18 @@ import z from "zod"
 	pk:z.string(),
 	sk:z.string(),
 	amountInStock:z.number().nonnegative(),
-	image:z.url(),
+	image: z.string().url("Invalid image URL"),
 	name:z.string().min(1, "Product name is required"),
-	price:z.number(),
+	price:z.number().positive("Price must be positive"),
 	type:z.string().min(1, "Product type is required")
 })
 export const productsArraySchema = z.array(productSchema);
+
+
+export const createProductSchema=z.object({
+	 name: z.string().min(1, "Product name is required"),
+  price: z.number().positive("Price must be positive"),
+ image: z.string().url("Invalid image URL"),
+  amountInStock: z.number().nonnegative("Stock cannot be negative"),
+  type: z.string().min(1, "Product type is required")
+})

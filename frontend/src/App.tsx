e27101e6a2ebf95db03c45bb6e4e73b9b81a2bfb
FBetulDemir <fatmabetuld@gmail.com>
2025-10-03 08:53:08 +0200

    Refactor Register component to handle registration errors/success and reset input fields to default when form submited

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./Pages/Home/Home";
 import Product from "./Pages/product/product";
 import Cart from "./Pages/cart/cart";
 import Login from "./Pages/user/Login.tsx";
+import Register from "./Pages/user/Register.tsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/product" element={<Product></Product>}></Route>
           <Route path="/cart" element={<Cart></Cart>}></Route>
           <Route path="/user/login" element={<Login />} />
+          <Route path="/user/register" element={<Register />} />
         </Routes>
       </HashRouter>
     </>
--- frontend/src/Pages/user/Register.tsx
+++ frontend/src/Pages/user/Register.tsx
@@ -6,56 +6,71 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setError("");
+    setSuccess("");
     try {
       const response = await fetch("api/users/register", {
         method: "POST",
         headers: { "content-type": "application/json" },
         body: JSON.stringify({ name, email, password }),
       });
+      if (!response.ok) {
+        const err = await response.json();
+        throw new Error(err.error || "Failed to register");
+      }
 
       const data = await response.json();
-    } catch (error) {}
+
+      setSuccess(`User created! ID: ${data.userId}`);
+      setName("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      setError((error as Error).message);
+    }
   };
-  return;
-  <div>
-    <h2>Registrera</h2>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Namn:</label>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Email:</label>
-        <br />
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Lösenord</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      {error && <div style={{ color: "red" }}>{error}</div>}
-      <button type="submit">Login</button>
-    </form>
-  </div>;
+  return (
+    <div>
+      <h2>Registrera</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Namn:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <br />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Lösenord</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        {error && <div style={{ color: "red" }}>{error}</div>}
+        {success && <p style={{ color: "green" }}>{success}</p>}
+        <button type="submit">Registrera</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;

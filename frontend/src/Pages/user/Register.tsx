492019b36099565edbd2767bbb9b0a548c06b736
FBetulDemir <fatmabetuld@gmail.com>
2025-10-03 08:39:30 +0200

    Implement Register component with form handling and error display

--- frontend/src/Pages/user/Register.tsx
+++ frontend/src/Pages/user/Register.tsx
@@ -1,6 +1,61 @@
+import type React from "react";
+import { useState } from "react";
+
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+    try {
+      const response = await fetch("api/users/register", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ name, email, password }),
+      });
+
+      const data = await response.json();
+    } catch (error) {}
+  };
   return;
-  <div></div>;
+  <div>
+    <h2>Registrera</h2>
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Namn:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Email:</label>
+        <br />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Lösenord</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      {error && <div style={{ color: "red" }}>{error}</div>}
+      <button type="submit">Login</button>
+    </form>
+  </div>;
 };
 
 export default Register;

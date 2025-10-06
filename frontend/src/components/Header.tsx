44806aef0adf031de3f3fc431e1c37c35333e3b4
FBetulDemir <fatmabetuld@gmail.com>
2025-10-06 08:27:26 +0200

    refactor Header component and styles for improved layout and responsiveness

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,21 +1,20 @@
-import "./Header.css"
-import logo from "../images/1cc7d577-c28b-480b-841d-c7b0d020a93d.png"
-import { NavLink } from "react-router-dom"
-function Header(){
-	return(
-		<div className="container-header">
-			<div>
-				<img src={logo} alt="logo" />
-
-			</div>
-			<div className="nav">
-				<NavLink to={"/"}>Home</NavLink>
-				<NavLink to={"/product"}>Products</NavLink>
-				<NavLink to={"/cart"}>Cart</NavLink>
-
-
-			</div>
-		</div>
-	)
+import "./Header.css";
+import logo from "../images/1cc7d577-c28b-480b-841d-c7b0d020a93d.png";
+import { NavLink } from "react-router-dom";
+function Header() {
+  return (
+    <div className="container-header">
+      <div className="logo-container">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="nav">
+        <NavLink to={"/"}>Hem</NavLink>
+        <NavLink to={"/product"}>Produkter</NavLink>
+        <NavLink to={"/cart"}>Varukorg</NavLink>
+        <NavLink to={"/register"}>Registrera dig</NavLink>
+        <NavLink to={"/login"}>Logga in</NavLink>
+      </div>
+    </div>
+  );
 }
-export default Header
+export default Header;
